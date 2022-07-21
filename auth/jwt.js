7000b1c085f872eb58a3dead490575b52a181870
HarshSutaria_vsspl <harshrajesh.sutaria@vasudhaika.net>
2022-07-21 15:39:34 +0530

    added a log

--- auth/jwt.js
+++ auth/jwt.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRouter = require("./jwt-user-implementation");
 const app = express();
 
-app.use(express.json()); // <==== parse request body as JSON
+app.use(express.json()); // <==== parse request body as JSONN
 
 app.use("/users", userRouter);
 app.listen(5001, (req, res) => {
