997022b01969abb43775d01f0d4873c475e14382
HarshSutaria_vsspl <harshrajesh.sutaria@vasudhaika.net>
2022-07-21 15:40:30 +0530

    added merger conflict

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const productsRouter = require("./products-seperate");
 const app = express();
 
-app.use(express.json()); // <==== parse request body as JSON
+app.use(express.json()); // <==== parse request body as JSONN
 
 app.use("/products", productsRouter);
 app.listen(5001, () => {
