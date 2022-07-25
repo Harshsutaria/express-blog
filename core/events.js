cb8156e996bbb55bffab2deac8ebc35fdb6d062e
HarshSutaria_vsspl <harshrajesh.sutaria@vasudhaika.net>
2022-07-25 12:44:18 +0530

    implemente base event in the class wayynode core/events.js!

--- core/events.js
+++ core/events.js
@@ -1,6 +1,19 @@
 // import core events module
 const EventEmitter = require("events");
-const event = new EventEmitter();
+
+// implementing event in oops way
+class Employee extends EventEmitter {
+  // initializing constructor
+  constructor() {
+    super();
+  }
+
+  hello() {
+    console.log("hello from the class hello method !!!");
+  }
+}
+
+const event = new Employee();
 
 // this is how we create a event using event.on method
 event.on("hello", () => {
@@ -15,6 +28,10 @@ event.on("hello", (name) => {
   console.log(`hello from ${name}!!!`);
 });
 
+// using class method as a callback while defining event
+event.on("ramu", event.hello);
+
 //calling the events like self call in-case of events e call emitting
 event.emit("hey");
 event.emit("hello", "charsh");
+event.emit("ramu");
