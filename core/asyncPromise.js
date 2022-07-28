df4331e4a937187d87f1aa58e9d2172022c599c1
HarshSutaria_vsspl <harshrajesh.sutaria@vasudhaika.net>
2022-07-28 19:54:02 +0530

    added promise initialization

---
+++ core/asyncPromise.js
@@ -0,0 +1,49 @@
+const { default: axios } = require("axios");
+
+//--------------------------------- handlying using promise -------------------------------------
+async function main() {
+  let userDetails;
+  console.log("hello");
+  // basic promise syntax
+  return new Promise((resolve, reject) => {
+    for (let name of ["harsh", "cristiano"]) {
+      console.log("1 is ");
+      try {
+        console.log("trying to you know fetch user information", name);
+        userDetails = axios.get("https://api.github.com/users/" + name);
+        console.log("promise resolved!!!");
+        resolve(userDetails);
+      } catch (error) {
+        console.log("getting error while fetching promise !!");
+        reject(error);
+      }
+    }
+  });
+}
+
+async function mainFunc() {
+  console.log("inside mainFunc");
+  console.log("1.");
+  console.log("2.");
+  console.log("3.");
+
+  let main2 = main()
+    .then((data) => {
+      console.log("data is ", data.data.name);
+      return data.data.name;
+    })
+    .catch((error) => {
+      console.log("error is wohoohohoohohoohohohooho");
+      return error;
+    });
+
+  // console.log("outside 4", main2);
+  console.log("outside 5");
+  return main2;
+}
+
+let a = mainFunc()
+  .then((data) => {
+    console.log("data is final outer");
+  })
+  .catch((err) => console.log(" outer **************** error is "));
