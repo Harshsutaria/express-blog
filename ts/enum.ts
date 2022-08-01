33bfa3ee6d1f50c9f8f59e2f8b29e10b3d599862
HarshSutaria_vsspl <harshrajesh.sutaria@vasudhaika.net>
2022-08-01 19:22:49 +0530

    updates in the stuff

---
+++ ts/enum.ts
@@ -0,0 +1,12 @@
+/**used to define some set of constants in ts  */
+/**
+ * Enums are one of the few features TypeScript has which is not a type-level extension of JavaScript.
+   Enums allow a developer to define a set of named constants. Using enums can make it easier 
+   to document intent, or create a set of distinct cases. TypeScript provides both numeric and string-based enums.
+ */
+enum language {
+  ENGLISH = "1",
+  HINDI = "2",
+}
+
+console.log(language.ENGLISH);
