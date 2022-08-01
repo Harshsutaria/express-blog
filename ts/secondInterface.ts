c4bbd3b7b768857690238264d214137a6416f804
HarshSutaria_vsspl <harshrajesh.sutaria@vasudhaika.net>
2022-08-01 17:39:46 +0530

    added generic in the ts implementation

--- ts/secondInterface.ts
+++ ts/secondInterface.ts
@@ -4,3 +4,19 @@ export interface secondInterface {
   mul(x: number, y: number): number;
   sub(x: number, y: number): number;
 }
+
+/**generics */
+
+function arrayConcat<T>(a: T[], b: T[]): T[] {
+  return a.concat(b);
+}
+
+let d = arrayConcat([1, 2, 3], [4, 5, 6]);
+console.log("d is ", d);
+d.push(7); //=> "working fine"
+// d.push('8') => throwing compilation error as the array is numbers only
+
+let f = arrayConcat([], ["h", "a"]);
+console.log("f is ", f);
+f.push("z"); //=> "working fine"
+// f.push(8) => throwing compilation error as the array is numbers only
